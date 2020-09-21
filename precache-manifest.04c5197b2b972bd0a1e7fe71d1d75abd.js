146ab920a2af4e722e43f14d682ce727998204e8
Xiuming Cui <cuixiuming2012@gmail.com>
2020-09-21 15:24:27 -0400

    Updates

---
+++ precache-manifest.04c5197b2b972bd0a1e7fe71d1d75abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aef6c25f6afa0769d90be8752a702d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463edd85f0370d1544a2",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8881005f5d7286eb79bc",
+    "url": "/static/js/2.559cd756.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.559cd756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463edd85f0370d1544a2",
+    "url": "/static/js/main.3142c4c3.chunk.js"
+  },
+  {
+    "revision": "e43c1db9632fec60ad54",
+    "url": "/static/js/runtime-main.6c209aa3.js"
+  },
+  {
+    "revision": "86c9d242e9ff1a65f9a1008e7d47f435",
+    "url": "/static/media/Resume_Xiuming_CUI.86c9d242.pdf"
+  }
+]);
